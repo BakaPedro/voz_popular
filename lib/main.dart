4a87ac5bc5a224b7f0fe522ced5169cfe65ef703
Pedro Henrique <pedro.1058.ph@gmail.com>
2025-06-11 17:31:32 -0300

    feat(login): Impressão de dados do campo de texto do login utilizando controllers

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:voz_popular/presentation/screens/login/login_screen.dart';
-import 'package:voz_popular/presentation/screens/login/login_screen.dart';
 import 'package:voz_popular/presentation/screens/register/register_screen.dart';
 import 'package:voz_popular/presentation/screens/home/home_screen.dart';
 import 'package:voz_popular/presentation/screens/profile/profile_screen.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -15,16 +15,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Voz Popular',
-      theme: ThemeData( // Seu tema continua aqui...
+      theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
         appBarTheme: const AppBarTheme(
           backgroundColor: Colors.deepPurple,
           foregroundColor: Colors.white,
-        )
+        ),
       ),
-      // home: const LoginScreen(), // REMOVA ou COMENTE esta linha
-      initialRoute: '/login', // Define a rota inicial
+      initialRoute: '/login',
       routes: {
         '/login': (context) => const LoginScreen(),
         '/register': (context) => const RegisterScreen(),
--- lib/presentation/screens/login/login_screen.dart
+++ lib/presentation/screens/login/login_screen.dart
@@ -1,20 +1,53 @@
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  //controller de texto
+  final _emailController = TextEditingController();
+  final _senhaController = TextEditingController();
+
+  //liberar memória
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _senhaController.dispose();
+    super.dispose();
+  }
+
+  void _login() {
+    //funcao login
+    final email = _emailController.text;
+    final senha = _senhaController.text;
+
+    //imprimir
+    print('Botão Entrar clicado!');
+    print('E-mail digitado: $email');
+    print('Senha digitada: $senha');
+
+    // navegação
+    Navigator.pushReplacementNamed(context, '/home');
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Login - Voz Popular')),
+      appBar: AppBar(
+        title: const Text('Login - Voz Popular'),
+      ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            // email
             TextFormField(
+              controller: _emailController, //associação
               decoration: const InputDecoration(
                 labelText: 'E-mail',
                 border: OutlineInputBorder(),
@@ -23,9 +56,8 @@ class LoginScreen extends StatelessWidget {
               keyboardType: TextInputType.emailAddress,
             ),
             const SizedBox(height: 16),
-
-            // senha
             TextFormField(
+              controller: _senhaController, //associação
               decoration: const InputDecoration(
                 labelText: 'Senha',
                 border: OutlineInputBorder(),
@@ -34,30 +66,23 @@ class LoginScreen extends StatelessWidget {
               obscureText: true,
             ),
             const SizedBox(height: 24),
-
-            // entrar
             ElevatedButton(
               style: ElevatedButton.styleFrom(
-                padding: const EdgeInsets.symmetric(vertical: 16),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8),
-                ),
+                  padding: const EdgeInsets.symmetric(vertical: 16),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8))),
+              onPressed: _login, // login()
+              child: const Text(
+                'Entrar',
+                style: TextStyle(fontSize: 16),
               ),
-              onPressed: () {
-                // acao inicio
-                Navigator.pushReplacementNamed(context, '/home');
-              },
-              child: const Text('Entrar', style: TextStyle(fontSize: 16)),
             ),
-            const SizedBox(height: 16),
-
-            // botao cadastro
             TextButton(
               onPressed: () {
                 Navigator.pushNamed(context, '/register');
               },
               child: const Text('Não tem uma conta? Cadastre-se'),
-            ),
+            )
           ],
         ),
       ),
